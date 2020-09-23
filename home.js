b0cae073382c95a80c975de1cb656af2b56a77c5
Bruno <brunoasobo@gmail.com>
2020-09-23 16:52:10 -0700

    Guess number game

---
+++ home.js
@@ -0,0 +1,13 @@
+// let emptyArray=[];
+// for (let num=0; num<10; num++){
+//     emptyArray.push(num);
+// }
+// console.log(emptyArray);
+
+let student={first:'Bruno',
+last:'asobo',
+age:36,
+height:6.1, 
+weight:180};
+
+console.log(student);
